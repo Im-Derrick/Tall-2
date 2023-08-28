450d26f17f4f154eb9c2933615fd0e0a5d7fe1bd
Im-Derrick <142809926+Im-Derrick@users.noreply.github.com>
2023-08-28 12:58:13 +0200

    Add files via upload

---
+++ tall.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int x=50;
+    int y=5;  //kan veksle mellom tallene når man vil for å beste hva man vil skal være størst
+    if (y>x) {
+        printf("y er større en x");
+    } else {
+        printf("x er større en y");
+    }
+    return 0;
+}
